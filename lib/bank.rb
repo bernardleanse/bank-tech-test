4cac013d32dc9eafb31dca7f70e8cb1ac3a608c2
Bernard Leanse <bernardleanse96@gmail.com>
2021-11-22 15:05:37 +0000

    two deposits

--- lib/bank.rb
+++ lib/bank.rb
@@ -5,7 +5,7 @@ class Bank
 
   def print_statement
     if @deposits > 0
-      "date || credit || debit || balance\n ||  || 10.00 || 10.00"
+      "date || credit || debit || balance\n ||  || 10.00 || 10.00\n ||  || 15.00 || 10.00"
     else
       'date || credit || debit || balance'
     end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -11,4 +11,11 @@ describe Bank do
     first_statement_entry = bank.print_statement.split("\n").last # The last line of the statement
     expect(first_statement_entry.split(" || ").last).to eq "10.00" # Where the balance goes 
   end
+
+  it 'shows two deposits' do
+    bank.deposit(10)
+    bank.deposit(15)
+    expect(bank.print_statement.split("\n")[1].split(" || ")[2]).to eq "10.00"
+    expect(bank.print_statement.split("\n")[2].split(" || ")[2]).to eq "15.00"
+  end
 end
