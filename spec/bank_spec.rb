24bcadf488b7e7e2cf39f87c79493b01bb13c40a
Bernard Leanse <bernardleanse96@gmail.com>
2021-11-22 11:32:18 +0000

    test for deposit and seeing on statement

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,4 +5,10 @@ describe Bank do
   it 'prints a statement' do
     expect(bank.print_statment).to eq('date || credit || debit || balance')
   end
+
+  it 'lets you deposit money and see it on the statement' do
+    bank.deposit(10)
+    first_statement_entry = bank.print_statment.split("\n").last # The last line of the statement
+    expect(deposit_statement_entry.split(" || ").last).to eq "10.00" # Where the balance goes 
+  end
 end
